5de604d55f6b25fb1f1c9ff88b5a7aa27315873e
Alice 3 <101829188+Alice1Kamui@users.noreply.github.com>
2023-03-12 18:56:04 -0300

    Update DailyPic.js

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -10,7 +10,7 @@ export default class DailyPicScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>DailyPic Screen!</Text>
+                <Text>Tela das Imagens Diárias!</Text>
             </View>
         )
     }
